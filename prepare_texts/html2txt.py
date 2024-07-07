5582cfe1fd17c4dbc51b3eff92222c7b77b2b426
Menelik Berhan <menelikberhan@gmail.com>
2024-07-07 18:04:43 +0000

    Add a for loop to go through files and call convert function then save to txt file

--- prepare_texts/html2txt.py
+++ prepare_texts/html2txt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Convert html and xml files to plain text files
+"""
 import os
 from glob import iglob
 
@@ -13,7 +16,7 @@ text_maker.ignore_images = True
 text_maker.ignore_tables = True     # Ignore table-related tags while keeping rows
 text_maker.ignore_links = True      # avoid hrefs
 text_maker.ignore_emphasis = True
-# text_maker.body_width = 78        # Wrap long lines at position. 0 for no wrapping.
+text_maker.body_width = 78        # Wrap long lines at position. 0 for no wrapping.
 # Use a single line break after a block element. Requires body width setting to be 0.
 # SINGLE_LINE_BREAK = False
 
@@ -23,12 +26,24 @@ strip_chars = utf_bom + '\n  #'     # '#' added for headings
 
 
 def convert_to_txt(convertor: html2text.HTML2Text, html_file_path: str):
+    """Convert html/xml to plain text file and save in same location"""
     with open(html_file_path, encoding='utf-8') as html_file:
         txt = convertor.handle(html_file.read())
-        print(txt.strip(strip_chars))     
-    convertor.close()
+        # print(txt.strip(strip_chars))
 
-pathname = os.path.join('./by_year', '*', '*.html')
+         # get path root except '.html/xml' extension
+        output_file_root = os.path.splitext(html_file_path)[0]
+        # use '.txt' extension for output file
+        output_file = output_file_root + '.txt'
+        with open(output_file, 'w') as file:
+            # strip unwanted chars & write to txt file
+            file.write(txt.strip(strip_chars))
 
-files_by_year = {}
-years = ['1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002']
+
+pathname = os.path.join('../training_texts', '**', '*.html')
+
+for html_file in iglob(pathname, recursive=True):
+    convert_to_txt(text_maker, html_file)
+    os.remove(html_file)
+
+text_maker.close()
