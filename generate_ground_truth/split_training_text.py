4f9a4e74f99d82141b891cd8cb99059be36a859e
Menelik Berhan <menelikberhan@gmail.com>
2024-07-16 01:50:47 +0000

    Edit: add fonts to use, and change args to text2image to suit 80 chars line.

--- generate_ground_truth/split_training_text.py
+++ generate_ground_truth/split_training_text.py
@@ -3,12 +3,39 @@
 # creates a text, image(tif) and box file for each line in a text file
 # output to be used as a ground truth for training tesseract
 
+from datetime import datetime
 import os
-import random
 import pathlib
+import random
 import subprocess
 
-training_text_file = 'training_text/kidane-introduction-clean.txt'
+from glob import iglob
+
+OVERWRITE_FILES = True
+training_text_file = './combined_80_chars_line.txt'
+
+output_directory = './amh-old-ground-truth'
+
+output_dir_path = pathlib.Path(output_directory)
+
+if not output_dir_path.is_dir():
+    os.mkdir(output_directory)
+else:
+    # move to backup folder
+    if list(iglob(output_dir_path.joinpath('*.txt').as_posix())) and\
+            not OVERWRITE_FILES:
+        t = datetime.now().strftime('%b-%d-%H:%M:%S')
+        move_to = pathlib.Path(f'./bak-ground-truth-{t}')
+        os.renames(output_directory, move_to)
+        os.mkdir(output_directory)
+        print(f'Moved old files to: "{move_to}"')
+    else:
+        # confirm overwrite
+        overwrite = input(
+            f'About To OVERWRITE files in"{output_dir_path}"\nYes(Y) | No(N): ')
+        if not overwrite in ['Yes', 'Y']:
+            exit(1)
+        print(f'Overwriting files in "{output_dir_path}"')
 
 lines = []
 
@@ -16,28 +43,48 @@ with open(training_text_file, 'r') as input_file:
     for line in input_file:
         lines.append(line.strip())
 
-output_directory = 'data/amh-old4-ground-truth'
-
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
-
-random.shuffle(lines)
+rand = random.Random(0)
+rand.shuffle(lines)
 
 # list of fonts to use (must be found in --fonts_dir param of text2image)
+# to install fonts: copy to `/usr/share/fonts` and `fc-cache -fv`
+# to list all available fonts: `fc-list : family style | sort`
 fonts = (
-    'Abyssinica SIL', 'Droid Sans Ethiopic', 'Droid Sans Ethiopic Bold',
-    'FreeSerif', 'Noto Sans Ethiopic', 'Noto Sans Ethiopic Bold')
+    # from okfonts of lstmdata/amh (except droid sans)
+    'Abyssinica SIL', 'FreeSerif',
+    'Noto Sans Ethiopic', 'Noto Sans Ethiopic Bold',
+    # from washera fonts
+    'Ethiopia Jiret', 'Ethiopic WashRa Bold, Bold', 'Ethiopic Wookianos',
+    'Ethiopic Zelan', 'Ethiopic Tint',
+    # from legally-free-geez-fonts-v1_0_0
+    'A0 Addis Abeba Unicode', 'Ethiopic Dire Dawa',
+)
+"""
+# omitted fonts (don't support all Ethiopic chars [esp. geez no.s]
+# or allowed non-Ethiopic like brackets)
 
+# from okfonts of lstmdata/amh (except droid sans)
+'Droid Sans Ethiopic'
+
+# from washera fonts
+'Ethiopic Fantuwua',    # very similar to 'Ethiopic Wookianos'
+
+# from legally-free-geez-fonts-v1_0_0
+"ahabesha'stypewriter", # geez no.s, brackets
+'Chiret',               # geez no.s, brackets
+'Geez Handwriting', 'Geez Handwriting Bold',    # geez no.s
+'Shiromeda',    # similar to 'a0-aa-unicode' & chars like `<<`
+"""
 # no of lines to process from txt file (comment out for all)
-count = 10
+count = 1000
 lines = lines[:count]
 
 # input file name without extention
 input_file_name = pathlib.Path(training_text_file).stem
 
 for font in fonts:
-    # remove space from font name
-    font_name = "-".join(font.split())
+    # remove space and comma from font name
+    font_name = "-".join([w.strip(',') for w in font.split()])
 
     # output files (box, text & image) base path for each font
     output_files_base_name = os.path.join(
@@ -48,6 +95,7 @@ for font in fonts:
         # output files (box, text & image) base path for each line
         line_files_base_name = f'{output_files_base_name}_{line_count}'
 
+        # write line to txt file
         line_training_text = f'{line_files_base_name}.gt.txt'
         with open(line_training_text, 'w') as output_file:
             output_file.writelines([line])
@@ -60,13 +108,25 @@ for font in fonts:
             f'--outputbase={line_files_base_name}',
             '--max_pages=1',
             '--strip_unrenderable_words',
-            '--leading=0',
-            '--xsize=1200',     # change xsize & ysize based on line length
-            '--ysize=80',       # +  to minimize margin spaces around text
-            '--char_spacing=0',
+            '--leading=32',      # Inter-line space (in pixels) default:12
+            '--xsize=3200',     # change xsize & ysize based on line length
+            '--ysize=480',       # +  to minimize margin spaces around text
+            '--char_spacing=0.2',  # Inter-character space in ems def:0
             '--exposure=0',
-            '--unicharset_file=langdata/amh.unicharset',
-            '--margin=0'
+            '--unicharset_file=unicharset_files/Ethiopic.unicharset',
+            # '--margin=0',
         ])
 
         line_count += 1
+
+        # subprocess.run([
+        #     'text2image',
+        #     '--max_pages=1',
+        #     '--strip_unrenderable_words',
+        #     '--leading=32',
+        #     '--xsize=3600',
+        #     '--ysize=480',
+        #     '--char_spacing=1.0',
+        #     '--exposure=0',
+        #     '--unicharset_file=langdata/eng.unicharset'
+        # ])
