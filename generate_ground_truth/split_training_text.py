e30c750d4bba0ee480bd447cf1739e4fb2886141
Menelik Berhan <minilikberhan@gmail.com>
2024-08-22 18:57:05 +0300

    Edit: first loop over fonts, and then lines for text2image. Add multiple fonts and make output files name more consice.

--- generate_ground_truth/split_training_text.py
+++ generate_ground_truth/split_training_text.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python3
 # source: https://github.com/astutejoe/tesseract_tutorial
-# creates a text, image(tif) and box file for each line in a text file
+# creates a text, image(tif) and box file for each line in a text file.
 # output to be used as a ground truth for training tesseract
 
 from datetime import datetime
 import os
 import pathlib
 import random
+import re
 import subprocess
 
 from glob import iglob
 
-OVERWRITE_FILES = True
-training_text_file = 'combined_w_corpus_80_l_80_chars_line.txt'
+OVERWRITE_FILES: "bool" = True
+training_text_file = "amh-layer.training_txt"
 
-output_directory = './amh-old-ground-truth'
+output_directory = "./amh-layer-ground-truth"
 
 output_dir_path = pathlib.Path(output_directory)
 
-if not output_dir_path.is_dir():
-    os.mkdir(output_directory)
-else:
-    # move to backup folder
-    if list(iglob(output_dir_path.joinpath('*.txt').as_posix())) and\
-            not OVERWRITE_FILES:
-        t = datetime.now().strftime('%b-%d-%H:%M:%S')
-        move_to = pathlib.Path(f'./bak-ground-truth-{t}')
-        os.renames(output_directory, move_to)
+
+def setup_output_dir(OVERWRITE_FILES, output_directory, output_dir_path):
+    if not output_dir_path.is_dir():
         os.mkdir(output_directory)
-        print(f'Moved old files to: "{move_to}"')
-    # else:
-    #     # confirm overwrite
-    #    overwrite = input(
-    #        f'About To OVERWRITE files in"{output_dir_path}"\nYes(Y) | No(N): ')
-    #    if not overwrite in ['Yes', 'Y']:
-    #        exit(1)
-    #    print(f'Overwriting files in "{output_dir_path}"')
+    else:
+        # move to backup folder
+        if (
+            list(iglob(output_dir_path.joinpath("*.txt").as_posix()))
+            and not OVERWRITE_FILES
+        ):
+            t = datetime.now().strftime("%b-%d-%H_%M_%S")
+            move_to = pathlib.Path(f"./bak-ground-truth-{t}")
+            os.renames(output_directory, move_to)
+            os.mkdir(output_directory)
+            print(f'Moved old files to: "{move_to}"')
+        else:
+            # confirm overwrite
+            overwrite = input(
+                f'About To OVERWRITE files in"{output_dir_path}"\nYes(Y) | No(N): '
+            )
+            if not overwrite in ["Yes", "Y"]:
+                exit(1)
+            print(f'Overwriting files in "{output_dir_path}"')
 
-lines = []
 
-with open(training_text_file, 'r') as input_file:
+setup_output_dir(OVERWRITE_FILES, output_directory, output_dir_path)
+
+lines: "list[str]" = []
+
+with open(training_text_file, "r") as input_file:
     for line in input_file:
         lines.append(line.strip())
 
@@ -49,98 +58,121 @@ rand.shuffle(lines)
 # list of fonts to use (must be found in --fonts_dir param of text2image)
 # to install fonts: copy to `/usr/share/fonts` and `fc-cache -fv`
 # to list all available fonts: `fc-list : family style | sort`
-fonts = (
+fonts = [
     # from okfonts of lstmdata/amh (except droid sans)
-    # 'Abyssinica SIL',
-	# 'FreeSerif',
-	'Noto Sans Ethiopic',
-    'Noto Sans Ethiopic Bold',
+    "Abyssinica SIL",
+    "FreeSerif",
+    "Noto Sans Ethiopic",
+    "Noto Sans Ethiopic Bold",
     # from washera fonts
-    'Ethiopia Jiret',
-    # 'Ethiopic WashRa Bold, Bold',
-    # 'Ethiopic Zelan',
+    "Ethiopia Jiret",
+    "Ethiopic WashRa Bold, Bold",
+    "Ethiopic WashRa SemiBold, Bold",
+    "Ethiopic Hiwua",  # 'Ethiopic Yebse', (very similar to Hiwua)
+    "Ethiopic Wookianos",
+    "Ethiopic Fantuwua",
+    "Ethiopic Tint",
     # # from legally-free-geez-fonts-v1_0_0
-    # 'A0 Addis Abeba Unicode', 'Ethiopic Dire Dawa',
-)
+    "Shiromeda",
+    "A0 Addis Abeba Unicode",
+]
+
+
+# no of lines to process from txt file (comment out for all)
+count = 10000
+lines = lines[:count]
+
+# input file name without extention
+input_file_name = pathlib.Path(training_text_file).stem
+
+# map of each font to its font name with space and comma replaced with `_`
+font_name_dict = {
+    font: re.sub(r"\,.+| ?Ethiopic ?", "", font).replace(" ", "_") for font in fonts
+}
+
+rand.shuffle(fonts)
+
+line_count = 1
+
+
+def parse_txt2img_log(line_no, font_name, output_files_base_name, result):
+    if result.stdout:
+        with open("./split_stdout", "a") as splt_out:
+            splt_out.write(result.stdout)
+    if result.stderr:
+        with open("./split_stderr", "a") as splt_out:
+            splt_out.write(result.stderr)
+
+    if f"{output_files_base_name}.tif" not in result.stderr:
+        with open("./skipped_err", "a") as err_file:
+            err_file.write(f"File: {output_files_base_name}\nErr: {result.stderr}\n")
+
+    if "Stripped" in result.stderr:
+        with open("./stripped_err", "a") as err_file:
+            err_file.write(f"File: {output_files_base_name}\nErr: {result.stderr}\n")
+
+    if result.returncode != 0:
+        with open("./split_error", "a") as err_file:
+            err_file.writelines(
+                [
+                    f"Line: {line_no},  Font: {font_name}\n",
+                    f"Stdout: {result.stdout}\n",
+                    f"Stderr: {result.stderr}\n",
+                ]
+            )
+
+
+for line in lines:
+    # output files (box, text & image) line no.
+    line_no = f"{line_count}"
+
+    for font in fonts:
+        # remove space and comma from font name & replace with `_`
+        font_name = font_name_dict[font]
+
+        # output files (box, text & image) base path for each line & font
+        output_files_base_name = os.path.join(
+            output_directory, f"{line_no}_{font_name}"
+        )
+
+        # write line to txt file
+        line_training_text = f"{output_files_base_name}.gt.txt"
+        with open(line_training_text, "w") as output_file:
+            output_file.writelines([line])
+
+        result = subprocess.run(
+            # subprocess.run(
+            [
+                "text2image",
+                f"--font={font}",
+                "--fonts_dir=../fonts",
+                f"--text={line_training_text}",
+                f"--outputbase={output_files_base_name}",
+                "--max_pages=1",
+                "--strip_unrenderable_words",
+                # "--leading=32",  # Inter-line space (in pixels) default:12
+                "--xsize=2400",  # change xsize & ysize based on line length
+                "--ysize=300",  # +  to minimize margin spaces around text
+                "--unicharset_file=unicharset_files/Ethiopic.unicharset",
+                "--distort_image=true",
+                "--exposure=-1",
+            ],
+            encoding="utf8",  # to force str format for result attributes
+            capture_output=True,  # to capture stdout & stderr
+        )
+        parse_txt2img_log(line_no, font_name, output_files_base_name, result)
+
+    line_count += 1
+
+
 """
 Font problems:
-'Ethiopic Wookianos' and 'Ethiopic Tint', dont render('በፕራይቬታይዜሽን ')
-"""
-"""
-# omitted fonts (don't support all Ethiopic chars [esp. geez no.s]
-# or allowed non-Ethiopic like brackets)
-
-# from okfonts of lstmdata/amh (except droid sans)
-'Droid Sans Ethiopic'
-
 # from washera fonts
-'Ethiopic Fantuwua',    # very similar to 'Ethiopic Wookianos'
+'Ethiopic Wookianos', 'Ethiopic Hiwua' and 'Ethiopic Tint', dont render ('ቨ - ቮ')
 
 # from legally-free-geez-fonts-v1_0_0
 "ahabesha'stypewriter", # geez no.s, brackets
 'Chiret',               # geez no.s, brackets
 'Geez Handwriting', 'Geez Handwriting Bold',    # geez no.s
-'Shiromeda',    # similar to 'a0-aa-unicode' & chars like `<<`
+'Shiromeda',    # similar chars like `<<`
 """
-# no of lines to process from txt file (comment out for all)
-# count = 1000
-# lines = lines[:count]
-
-# input file name without extention
-input_file_name = pathlib.Path(training_text_file).stem
-
-# TODO: check if first loopin ove lines then fonts have difference
-for font in fonts:
-    # remove space and comma from font name
-    font_name = "-".join([w.strip(',') for w in font.split()])
-
-    # output files (box, text & image) base path for each font
-    output_files_base_name = os.path.join(
-    output_directory, f'{input_file_name}_{font_name}')
-    if font == 'Noto Sans Ethiopic':
-        line_count = 194424
-        lines_custom = lines[194424:]
-    else:
-        line_count = 0
-        lines_custom = lines
-    for line in lines_custom:
-        # output files (box, text & image) base path for each line
-        line_files_base_name = f'{output_files_base_name}_{line_count}'
-
-        # write line to txt file
-        line_training_text = f'{line_files_base_name}.gt.txt'
-        with open(line_training_text, 'w') as output_file:
-            output_file.writelines([line])
-
-        subprocess.run([
-            'text2image',
-            f'--font={font}',
-            '--fonts_dir=../fonts',
-            f'--text={line_training_text}',
-            f'--outputbase={line_files_base_name}',
-            '--max_pages=1',
-            '--strip_unrenderable_words',
-            '--leading=32',      # Inter-line space (in pixels) default:12
-            '--xsize=3400',     # change xsize & ysize based on line length
-            '--ysize=480',       # +  to minimize margin spaces around text
-            '--char_spacing=0.1',  # Inter-character space in ems def:0
-            '--exposure=0',
-            '--unicharset_file=unicharset_files/Ethiopic.unicharset',
-            # '--margin=0',
-        ])
-
-        line_count += 1
-
-    # if font == 'Abyssinica SIL':
-    #     break
-        # subprocess.run([
-        #     'text2image',
-        #     '--max_pages=1',
-        #     '--strip_unrenderable_words',
-        #     '--leading=32',
-        #     '--xsize=3600',
-        #     '--ysize=480',
-        #     '--char_spacing=1.0',
-        #     '--exposure=0',
-        #     '--unicharset_file=langdata/eng.unicharset'
-        # ])
