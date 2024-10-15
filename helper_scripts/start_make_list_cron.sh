c99dfe3404e78fe490737217d80eae6107822ef1
Menelik Berhan <minilikberhan@gmail.com>
2024-10-15 21:53:27 +0300

    Edit: add a copy script to run two txt2image processes

--- helper_scripts/start_make_list_cron.sh
+++ helper_scripts/start_make_list_cron.sh
@@ -2,15 +2,25 @@
 # Script to check if make is running & if not start it.
 # To be used for a cron job using corntab -e
 
+
 a="$(pgrep make)"
 if [ "$?" == 0 ] ; then
-	echo "$(date) : Running"
+        echo "$(date) : Running"
 else
-	echo "$(date) : Not Running"
-	cd /home/menelikberhan/train-tesseract/tesstrain && \
-	source ../.venv/bin/activate && \
-	OMP_THREAD_LIMIT=1 TESSDATA_PREFIX=../data/tessdata make lists \
-	MODEL_NAME=amh-layer START_MODEL=amh DATA_DIR=../data \
-	TESSDATA=../data/tessdata LEARNING_RATE=0.0001 \
-	RATIO_TRAIN=0.999 EPOCHS=1 >> ../out_create_lstmf_chron 2>&1
+        echo "$(date) : Not Running"
+
+        # check if `make` was killed by OOM
+        killed_info="$(dmesg -T | grep kill | grep make)"
+
+        # restart `make`
+        if [ "$?" == 0 ] ; then
+                echo "$(date) : Killed by OOM: $killed_info"
+
+		source ../.venv/bin/activate && \
+
+		TESSDATA_PREFIX=../data/tessdata make lists \
+		MODEL_NAME=amh-layer START_MODEL=amh \
+		DATA_DIR=../data TESSDATA=../data/tessdata \
+		EPOCHS=1 >> ../out_perpare_train_data_chron_1 2>&1
+	fi
 fi
